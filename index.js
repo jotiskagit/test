0b53daff70686cda675dbd1135957f84f55a9350
jotiskagit <135152118+jotiskagit@users.noreply.github.com>
2024-08-27 20:03:30 +0530

    changed html code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function exportTableToCSV() {
-    let table = document.getElementById
+function tableToCSV() {
+    let table = document.getElementById("table1");
+    let row = table.querySelectorAll("tr");
+    let csvContent = "";
+    
 }
